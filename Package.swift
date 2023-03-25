21c871536adcc203ccfc518e1d20eddf5ab7ded7
Guerson <ultim8p@gmail.com>
2023-03-25 06:06:05 +0100

    remove iOS

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "NoCrypto",
     platforms: [
-        .iOS(.v13),
-        .macOS(.v12)
+        .macOS(.v12),
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
